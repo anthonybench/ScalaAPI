5c5524f04c4fdd3f002ff209df308e2b6a155aa8
Isaac Yep <anthonybenchyep@gmail.com>
2021-02-20 17:49:53 -0800

    jafsdkl

--- play-samples-play-scala-hello-world-tutorial/app/controllers/HomeController.scala
+++ play-samples-play-scala-hello-world-tutorial/app/controllers/HomeController.scala
@@ -4,6 +4,10 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+import scala.collection.mutable
+import play.api.libs.json._
+import models._
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
@@ -40,4 +44,24 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def getTest() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.test())
   }
+}
+
+
+// Controller
+@Singleton
+class VillagersController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+    private val villagers = new mutable.ListBuffer[Villager]()
+    villagers += Villager(1, lazyy, horse, "May 1st", "clip clawp", play)
+    villagers += Villager(2, peppy, tiger, "August 27th", "growf", fashion)
+
+    implicit val villagerJson = Json.format[Villager]
+
+    def getAll() = Action { implicit request: Request[AnyContent] =>
+        if (Villagers.isEmpty) {
+            NoContent
+        } else {
+            Ok(Json.toJason(villagers))
+        }
+    }
 }
